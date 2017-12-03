3d7adb126762a4cf78054bf257ffcfb5db6cb384
Stanislav Nedelchev <stan@stannedelchev.net>
2017-12-03 15:21:14 +0200

    Add simple Day2 solutions

---
+++ AdventOfCode2017/Day2/Solver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Day2
+{
+    public class Solver
+    {
+        public int SolvePart1(string input)
+        {
+            var result = this.CalculateChecksum(input, ints => ints.Max() - ints.Min());
+            return result;
+        }
+
+        public int SolvePart2(string input)
+        {
+            var result = this.CalculateChecksum(input, EvenlyDivisibleChecksum);
+            return result;
+        }
+
+        private int CalculateChecksum(string input, Func<IEnumerable<int>, int> rowChecksumFunc)
+        {
+            var result = input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Split(" \t".ToCharArray())
+                .Select(int.Parse))
+                .Select(rowChecksumFunc)
+                .Sum();
+            return result;
+        }
+
+        private int EvenlyDivisibleChecksum(IEnumerable<int> args)
+        {
+            var ints = args.ToArray();
+            for (int i = 0; i < ints.Length; i++)
+            {
+                for (int j = 0; j < ints.Length; j++)
+                {
+                    var largerNumber = Math.Max(ints[i], ints[j]);
+                    var smallerNumber = Math.Min(ints[i], ints[j]);
+                    if (largerNumber % smallerNumber == 0 && largerNumber != smallerNumber)
+                    {
+                        return largerNumber / smallerNumber;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -8,9 +8,13 @@ namespace AdventOfCode2017
         {
             var day1Part1Solution = new Day1.Solver().SolvePart1(File.ReadAllText(@"Inputs\Day1_input.txt"));
             var day1Part2Solution = new Day1.Solver().SolvePart2(File.ReadAllText(@"Inputs\Day1_input.txt"));
+            var day2Part1Solution = new Day2.Solver().SolvePart1(File.ReadAllText(@"Inputs\Day2_input.txt"));
+            var day2Part2Solution = new Day2.Solver().SolvePart2(File.ReadAllText(@"Inputs\Day2_input.txt"));
 
             System.Console.WriteLine($"Day 1.1 : {day1Part1Solution}");
             System.Console.WriteLine($"Day 1.2 : {day1Part2Solution}");
+            System.Console.WriteLine($"Day 2.1 : {day2Part1Solution}");
+            System.Console.WriteLine($"Day 2.2 : {day2Part2Solution}");
         }
     }
 }
